08bd58fa76e49acf073159c3bcff763db3c13b26
Steven Espinal <stevenjesusespinal.com>
2020-06-20 04:46:39 -0400

    fetched data from firestore and displayed in link list

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
+import FirebaseContext from "../../firebase/context";
+import LinkItem from "./LinkItem";
+
 
 function LinkList(props) {
-  return <div>LinkList</div>;
+  const {firebase} = useContext(FirebaseContext);
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    getLinks()
+  }, []);
+
+  function getLinks() {
+    firebase.db.collection('links').onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+    console.log(links);
+    setLinks(links);
+  }
+
+  return (
+    <div>
+      {links.map((link, i) => (
+        <LinkItem key={link.id} showCount={true} link={link} index={i + 1}/>
+      ))}
+    </div>
+  );
 }
 
 export default LinkList;
